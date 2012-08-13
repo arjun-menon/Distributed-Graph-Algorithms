f3183c1a7bbf0e210bd7fa512af2dd9eba123e90
Arjun G. Menon <contact@arjungmenon.com>
2012-08-13 12:32:39 +0400

    Moed DistAlgo sources, created Eclipse project

--- DistAlgo-0.2/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-
-setup(name = "DistAlgo",
-      version = "0.2",
-      author= "bolin",
-      author_email = "bolin@cs.stonybrook.edu",
-      packages = ['distalgo', 'distalgo.compiler', 'distalgo.runtime', 'pympler'])
