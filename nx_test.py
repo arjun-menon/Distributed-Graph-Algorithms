6637235e6a837e33def98f723f23eb278c38c3f7
Arjun G. Menon <contact@arjungmenon.com>
2012-08-15 19:47:36 +0400

    slowly adding parallelism

--- nx_test.py
+++ nx_test.py
@@ -1,5 +1,22 @@
 
 import networkx as nx
+from collections import deque
 
-G=nx.Graph()
+#q = deque()
+#
+#q.appendleft(1)
+#q.appendleft(2)
+#q.appendleft(3)
+#
+#print(q)
+#
+#print(q.pop())
 
+G = nx.balanced_tree(2, 3)
+
+print(G.nodes())
+print(G.edges())
+
+print(set( G.neighbors(1) ))
+
+print(G[0])
