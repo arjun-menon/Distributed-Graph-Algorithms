c71841d62ec2b4b674e8aed142d80d2e108a6ba8
Arjun G. Menon <contact@arjungmenon.com>
2012-08-20 02:32:01 +0400

    moved to dir BFS

---
+++ BFS/run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import sys
+from distalgo.runtime import *
+
+sys.argv = [ sys.argv[0], "BFS.dis"]
+
+libmain()
--- run.py
+++
@@ -1,8 +0,0 @@
-
-import sys
-from distalgo.runtime import *
-
-#sys.argv = [ sys.argv[0], "BFS.dis"]
-sys.argv = [ sys.argv[0], "MST.dis"]
-
-libmain()
