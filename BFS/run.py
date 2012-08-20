75ecd966ecb55a8dca49c818b6da58403782fed7
Arjun G. Menon <contact@arjungmenon.com>
2012-08-20 05:36:50 +0400

    run.py (minor)

--- BFS/run.py
+++ BFS/run.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import sys
+sys.path.append("..") # if DistAlgo is not installed, use the one in parent directory
+
 from distalgo.runtime import *
 
 sys.argv = [sys.argv[0], "BFS.dis"]
