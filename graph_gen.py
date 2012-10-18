2bc7e0e0de8cfa800894d91a497e61f1b62bcd88
Arjun G. Menon <contact@arjungmenon.com>
2012-10-18 05:05:17 +0400

    Added networkx_random_weighted_graph(n, e) and other changes

--- graph_gen.py
+++ graph_gen.py
@@ -29,16 +29,16 @@ def enumerate_edges(n):
             e += [(i,j)]
     return e
 
-def random_graph(max_nodes, edge_count):
+def random_edges(max_nodes, edge_count):
     all_edges = enumerate_edges(max_nodes)
 
     return random.sample(all_edges, edge_count)
 
-def random_weighted_graph(max_nodes, edge_count):
+def random_weighted_edges(max_nodes, edge_count):
     def edge(n1, n2, w):
             return (n1, n2, {'weight':w})
 
-    rg = random_graph(max_nodes, edge_count)
+    rg = random_edges(max_nodes, edge_count)
 
     random_weights = random.sample(range(1, len(rg)*10), len(rg))
 
@@ -50,11 +50,34 @@ def random_weighted_graph(max_nodes, edge_count):
 
     return edges
 
-def weighted_graph_to_file(g, fname):
+def weighted_edges_to_file(g, fname):
     with open(fname, 'w') as f:
         for e in g:
             u, v, w = e
             f.write("%s %s %d\n" % (u, v, w['weight']))
+
+def networkx_random_weighted_graph(n, e):
+    """Generate a random NetworkX graph.
+
+    n : The maximum number of nodes in the graph.
+    e : The number of edges in the graph.
+    """
+
+    import sys
+    sys.path.append('..')
+    import networkx as nx
+
+    G = nx.Graph()
+
+    G.add_edges_from( random_weighted_edges(n, e) )
+
+    random_attributes = random.sample(range(1, n*10), n)
+    r_i = 0
+    for n in G.nodes():
+        G.node[n]['value'] = random_attributes[ r_i ]
+        r_i += 1
+
+    return G
 
 if __name__ == "__main__":
     import argparse
@@ -64,13 +87,14 @@ if __name__ == "__main__":
     parser.add_argument('file_name', nargs='?', type=str, default='random_graph', help='File to write the graph to, listing the edges of a graph line-by-line in the following style: "A B 2", where "A" and "B" are node names and "2" is the weight of the edge connecting them.')
     args = parser.parse_args()
     n = args.nodes[0]
-    print("Maximum number of edges is %d." % max_edges(n))
+    print("Maximum possible number of edges is %d." % max_edges(n))
     e = args.edges[0]
     print("Out of which %d edges will be randomly chosen." % e)
     f = args.file_name
 
-    rg = random_weighted_graph(n, e)
-
-    weighted_graph_to_file(rg, f)
-
+    rg = random_weighted_edges(n, e)
     print(rg)
+    weighted_edges_to_file(rg, f)
+
+    ng = networkx_random_weighted_graph(n, e)
+    print(ng.nodes(data=True))
